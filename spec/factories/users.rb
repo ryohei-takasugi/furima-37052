6d037f27b6446794cc97500a596fca1d8ae0a590
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-10-18 17:00:46 +0900

    Spec ユーザー管理機能の実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,14 @@
 FactoryBot.define do
+  name = Gimei.new
   factory :user do
+    email                 { Faker::Internet.email }
+    password              { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
+    password_confirmation { password }
+    nickname              { name.last.hiragana }
+    last_name             { name.last.kanji }
+    last_name_kana        { name.last.katakana }
+    first_name            { name.first.kanji }
+    first_name_kana       { name.first.katakana }
+    birthday              { Faker::Date.birthday(min_age: 18, max_age: 65) }
   end
 end
