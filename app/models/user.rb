cf8d9dba8ffcb1f7d12db6ff03a7b5aec3a04d20
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-10-18 16:59:57 +0900

    バリデーションにカナの判定を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
+  KANA_REGEX = /\A[ァ-ヶ]+\z/.freeze
   with_options presence: true do
     validates :nickname
-    validates :last_name,          format: { with: NAME_REGEX, message: 'には全角を入力してください' }
-    validates :last_name_kana,     format: { with: NAME_REGEX, message: 'には全角を入力してください' }
-    validates :first_name,         format: { with: NAME_REGEX, message: 'には全角を入力してください' }
-    validates :first_name_kana,    format: { with: NAME_REGEX, message: 'には全角を入力してください' }
+    validates :last_name,          format: { with: NAME_REGEX, message: 'には全角（漢字・ひらがな・カタカナ）を入力してください' }
+    validates :last_name_kana,     format: { with: KANA_REGEX, message: 'には全角カタカナを入力してください' }
+    validates :first_name,         format: { with: NAME_REGEX, message: 'には全角（漢字・ひらがな・カタカナ）を入力してください' }
+    validates :first_name_kana,    format: { with: KANA_REGEX, message: 'には全角カタカナを入力してください' }
     validates :birthday
   end
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }
 end
