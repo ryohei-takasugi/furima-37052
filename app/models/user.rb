49b8681dea7139582771391ba0617cd6ce9a3e8e
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-10-18 15:07:26 +0900

    新規登録＆ログイン画面の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
+  with_options presence: true do
+    validates :nickname
+    validates :last_name,          format: { with: NAME_REGEX, message: 'には全角を入力してください' }
+    validates :last_name_kana,     format: { with: NAME_REGEX, message: 'には全角を入力してください' }
+    validates :first_name,         format: { with: NAME_REGEX, message: 'には全角を入力してください' }
+    validates :first_name_kana,    format: { with: NAME_REGEX, message: 'には全角を入力してください' }
+    validates :birthday
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
