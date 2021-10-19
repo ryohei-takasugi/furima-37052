380604f31588d9e7554fd9dd9953eef6ca377245
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-10-19 12:29:10 +0900

    商品出品画面の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,28 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    item = Item.new(item_params)
+    puts "item_params[:price]: #{item_params[:price]}"
+    puts "item.price         : #{item.price}"
+    if item.valid? && item.save
+      redirect_to root_path
+    else
+      @item = item
+      render :new
+    end
+  end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 end
