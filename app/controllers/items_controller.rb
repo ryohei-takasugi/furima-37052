356328eb48e67a7735e53e1f3a8572c87041ce79
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-10-19 16:38:14 +0900

    コードレビューの指摘事項を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,11 +9,10 @@ class ItemsController < ApplicationController
   end
 
   def create
-    item = Item.new(item_params)
-    if item.valid? && item.save
+    @item = Item.new(item_params)
+    if @item.save
       redirect_to root_path
     else
-      @item = item
       render :new
     end
   end
