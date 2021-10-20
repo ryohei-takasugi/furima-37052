879ccc0706f2fc43cd6f83c8f4ee38aae78cde8e
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-10-20 18:33:38 +0900

    商品購入画面の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,33 @@
 class OrdersController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @order_delivery = OrderDelivery.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_delivery = OrderDelivery.new(order_params)
+    if @order_delivery.valid?
+      pay_item
+      @order_delivery.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_delivery).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(token: params[:token]).merge(price: @item.price).merge(item_id: params[:item_id]).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
---
+++ app/models/order_delivery.rb
@@ -0,0 +1,19 @@
+class OrderDelivery
+  include ActiveModel::Model
+  attr_accessor :token, :price, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :token
+    validates :price
+    validates :postal_code,   format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :city
+    validates :addresses
+    validates :phone_number,  numericality: { only_integer: true, message: 'is invalid. Input only number' }, length: { in: 10..11, message: 'is too short' }
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Delivery.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
+  end
+end
